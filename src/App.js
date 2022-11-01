978554c2cd9b58ba05be3941c27a03c821b2d6a2
Stu Bolderson <stu.bolderson@aol.com>
2022-11-01 19:26:14 +0100

    base router link between !user and user working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+// npm
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// files
+import Home from "./pages/Home/Home";
+import Welcome from "./pages/Welcome/Welcome";
+
 function App() {
+  const user = "";
   return (
     <div className="App">
-      <h1>Netflix</h1>
+      <Router>
+        <Routes>
+          {!user && <Route path="/" element={<Welcome />} />}
+          {user && <Route path="/" element={<Home />} />}
+        </Routes>
+      </Router>
     </div>
   );
 }
