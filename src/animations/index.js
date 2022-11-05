ad736bcbd4187ab5049cfe504baf9f89c75a45f0
Stu Bolderson <stu.bolderson@aol.com>
2022-11-05 12:52:12 +0100

    motion.nav working - refactor variants

---
+++ src/animations/index.js
@@ -0,0 +1,4 @@
+export const navVarient = {
+  visible: { y: 0, opacity: 1 },
+  hidden: { y: -10, opacity: 0 },
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 // npm
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { motion, useScroll } from "framer-motion";
 
 // files
 import logo from "../assets/images/netflix_logo.png";
@@ -9,14 +11,43 @@ import avatar from "../assets/images/netflix_avatar.png";
 import styles from "../styles/Nav.module.css";
 
 export default function Navbar() {
+  // local state
+  const [show, setShow] = useState(false);
+
+  // properties
+  const { scrollY } = useScroll();
+
+  const variants = {
+    visible: { y: 0, opacity: 1 },
+    hidden: { y: -10, opacity: 0 },
+  };
+
+  // methods
+  function updateScrollPos() {
+    if (scrollY?.current < scrollY.prev) {
+      setShow(false);
+    } else if (scrollY.current > 100 && scrollY?.current > scrollY?.prev) {
+      setShow(true);
+    }
+  }
+
+  useEffect(() => {
+    return scrollY.onChange(() => updateScrollPos());
+  }, []);
+
   return (
-    <nav className={styles.nav__main}>
+    <motion.nav
+      className={styles.nav__main}
+      variants={variants}
+      animate={show ? "visible" : "hidden"}
+      transition={{ ease: "linear", duration: 0.3 }}
+    >
       <Link to={"/"}>
         <img src={logo} alt="netflix logo" className={styles.nav__logo} />
       </Link>
       <Link to={"/"}>
         <img src={avatar} alt="netflix avatar" className={styles.nav__avatar} />
       </Link>
-    </nav>
+    </motion.nav>
   );
 }
