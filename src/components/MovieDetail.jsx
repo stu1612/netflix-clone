3a70821b5ed34cda043cea82374a0859d6142604
Stu Bolderson <stu.bolderson@aol.com>
2022-11-08 16:39:00 +0100

    updated fetchMovie function to .then - fix reference error

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,26 +1,42 @@
 // npm
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 // styles
 import styles from "../styles/Movies.module.css";
 
 export default function MovieDetail({ id }) {
+  // local state
+  const [movieData, setMovieData] = useState();
+  // properties
+  const api_image_url = "https://image.tmdb.org/t/p/original/";
+
+  // methods
   useEffect(() => {
     async function fetchMovie(id) {
-      const data = await axios.get(
-        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`
-      );
-      console.log(data);
+      await axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`
+        )
+        .then((res) => {
+          setMovieData(res);
+        });
     }
     fetchMovie(id);
   }, [id]);
 
+  console.log(movieData);
+
+  // const { backdrop_path } = movieData.data;
+
   return (
     <div className={styles.container__outer}>
       <div className={styles.container__inner}>
-        <h2>{id} number</h2>
+        <div className={styles.container__image}>
+          {/* <img src={`${api_image_url}${backdrop_path}`} alt="" /> */}
+        </div>
       </div>
+      <div className={styles.container__content}></div>
     </div>
   );
 }
