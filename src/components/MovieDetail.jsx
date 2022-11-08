63bd5fad08a05093873a3a9546a20081f676aed2
Stu Bolderson <stu.bolderson@aol.com>
2022-11-08 12:57:08 +0100

    step 3 - fetch request for movie in MovieDetail component consoles data

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,7 +1,21 @@
+// npm
+import { useEffect } from "react";
+import axios from "axios";
+
 // styles
 import styles from "../styles/Movies.module.css";
 
 export default function MovieDetail({ id }) {
+  useEffect(() => {
+    async function fetchMovie(id) {
+      const data = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`
+      );
+      console.log(data);
+    }
+    fetchMovie(id);
+  }, [id]);
+
   return (
     <div className={styles.container__outer}>
       <div className={styles.container__inner}>
