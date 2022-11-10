84b3f2bfceeb1229a1bc459b44488fc2cb848d00
Stu Bolderson <stu.bolderson@aol.com>
2022-11-10 19:32:52 +0100

    media detail gradient added

--- src/components/MediaDetail.jsx
+++ src/components/MediaDetail.jsx
@@ -15,40 +15,57 @@ export default function MediaDetail({ media }) {
   // properties
   const api_image_url = "https://image.tmdb.org/t/p/original/";
 
+  // const { backdrop_path } = media;
+
   // const { title, vote_average, release_date, runtime, first_air_date } =
   //   mediaDetail;
 
-  console.log(media);
-
-  return (
-    <div></div>
-    // <div className={styles.container__outer}>
-    //   <div className={styles.container__inner}>
-    //      <div className={styles.media__image}>
-    //       <img src={`${api_image_url}${mediaDetail.backdrop_path}`} alt="" />
-    //     </div>
-    //      <div className={styles.media__content}>
-    //       <div>
-    //         <div>
-    //           <h1 className={styles.headingH1}>{title}</h1>
-    //           <span>
-    //             {subString(release_date || first_air_date)} |{" "}
-    //             {convertTime(runtime)}
-    //           </span>
-    //         </div>
-    //         <span>
-    //           {toFixed(vote_average)} <AiFillStar />
-    //         </span>
-    //       </div>
-    //       <Tabs mediaDetail={mediaDetail} />
-    //       <div>
-    //         <h3>
-    //           staring <span></span>
-    //         </h3>
-    //       </div>
-    //       <span className={styles.close}>X</span>
-    //     </div>
-    //    </div>
-    //  </div>
-  );
+  return media ? (
+    <div className={styles.container__outer}>
+      <div className={styles.container__inner}>
+        <div
+          style={{
+            backgroundImage: `url(${api_image_url}${media.backdrop_path})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            backgroundPosition: "90%",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          {/* <img src={`${api_image_url}${media.backdrop_path}`} alt="" /> */}
+        </div>
+        <div className={styles.bg}></div>
+      </div>
+    </div>
+  ) : null;
 }
+
+// <div className={styles.container__outer}>
+//   <div className={styles.container__inner}>
+//     <div className={styles.media__image}>
+//       <img src={`${api_image_url}${backdrop_path}`} alt="" />
+//     </div>
+//     <div className={styles.media__content}>
+//       <div>
+//         <div>
+//           <h1 className={styles.headingH1}>{title}</h1>
+//           <span>
+//             {subString(release_date || first_air_date)} |{" "}
+//             {convertTime(runtime)}
+//           </span>
+//         </div>
+//         <span>
+//           {toFixed(vote_average)} <AiFillStar />
+//         </span>
+//       </div>
+//       <Tabs mediaDetail={mediaDetail} />
+//       <div>
+//         <h3>
+//           staring <span></span>
+//         </h3>
+//       </div>
+//       <span className={styles.close}>X</span>
+//     </div>
+//   </div>
+// </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,9 @@
 // npm
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 
 // files
 import styles from "../styles/Movies.module.css";
-import slugifyString from "../utils/slugifyString";
 import { Modal } from "./Modal";
 import useModal from "../hooks/useModal";
 import MediaDetail from "./MediaDetail";
@@ -15,7 +13,6 @@ export default function MovieCard({ item, movie }) {
   // properties
   const api_image_url = "https://image.tmdb.org/t/p/original/";
   const { poster_path, name, title, id } = item;
-  const slug = slugifyString(title || name);
   const { isShowing, toggle } = useModal();
   const isMovie = movie;
 
@@ -34,38 +31,16 @@ export default function MovieCard({ item, movie }) {
     });
   }
 
-  //   async function fetchMovie(id) {
-  //     const url = isMovie
-  //       ? `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`
-  //       : `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`;
-  //     await axios.get(url).then((res) => {
-  //       const obj = res.data;
-  //       setMediaDetail(obj);
-  //     });
-  //   }
-  //   fetchMovie(state);
-  // }, [state]);
-
   return (
     <>
-      {/* <Link
-        className={styles.poster}
-        to={`overview/${slug}`}
-        state={[id, movie]}
-      > */}
       <img
         src={`${api_image_url}${poster_path}`}
         alt={name || title}
         className={styles.poster__image}
         onClick={() => handleClick()}
       />
-      {/* </Link> */}
       <Modal isShowing={isShowing}>
-        <div
-          style={{ backgroundColor: "yellow", height: "90vh", width: "100%" }}
-        >
-          <MediaDetail media={media} />
-        </div>
+        <MediaDetail media={media} />
       </Modal>
     </>
   );
